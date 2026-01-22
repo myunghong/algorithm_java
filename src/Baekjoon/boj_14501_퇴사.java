5c708f284e4d9e2e4c21b1c55a1106a96213d7e7
홍서준 <auddml321@hanyang.ac.kr>
2026-01-22 15:43:18 +0900

    boj_14501_퇴사

--- src/Baekjoon/boj_14501_퇴사.java
+++ src/Baekjoon/boj_14501_퇴사.java
@@ -7,7 +7,8 @@ import java.util.StringTokenizer;
 public class boj_14501_퇴사 {
 	static int[] day = new int[16];
 	static int[] money = new int[16];
-	
+	static int ans = 0;
+	static int[] result = new int[16];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int TC = Integer.parseInt(br.readLine());
@@ -15,13 +16,37 @@ public class boj_14501_퇴사 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			if(tc + n > TC)
+			if(tc + n > TC) {
+				day[tc] = -1;
+				money[tc] = -1;
+				continue;
+			}
 			day[tc] = n;
 			
 			money[tc] = m;
 		}
 		
-	
-	
+		for(int i = TC-1; i>=0; i--) {
+			int temp = 0;
+			if(day[i] == -1) {
+				continue;
+			} else {
+				for(int j = i + day[i]; j<TC; j++) {
+					temp = Math.max(temp, money[j]);
+							
+				}
+				money[i] += temp;
+			}
+			
+			
+			
+		}
+		for(int i = 0; i<TC; i++) {
+			
+			ans = Math.max(ans,money[i]);
+		}
+		
+		System.out.println(ans);
+		return;
 	}
 }
