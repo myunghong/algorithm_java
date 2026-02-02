e74ac24ba0658a9c27bc70f778d960a35a05e323
홍서준 <auddml321@hanyang.ac.kr>
2026-02-02 11:29:09 +0900

    boj_11724_연결요소의개수 (그래프): 틀려서 수정

--- src/Baekjoon/boj_11724_연결요소의개수.java
+++ src/Baekjoon/boj_11724_연결요소의개수.java
@@ -25,21 +25,19 @@ public class boj_11724_연결요소의개수 {
 		int[] vis = new int[N+1];
 		
 		for(int i = 1; i<=N; i++) {
-			int cur = i, temp = 0;
 			
 			if(vis[i] !=0) continue;
 			Q.offer(i);
 			vis[i] = 1;
+			ans++;
 			while(!Q.isEmpty()) {
-				cur = Q.poll();
+				int cur = Q.poll();
 				for(int j = 0; j<arr[cur].size(); j++) {
 					if(vis[arr[cur].get(j)] != 0) continue;
 					Q.offer(arr[cur].get(j));
 					vis[arr[cur].get(j)] = 1;
-					temp++;
 				}
 			}
-			if(temp != 0) ans++;
 			
 			
 			
