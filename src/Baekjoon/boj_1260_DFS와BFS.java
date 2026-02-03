e7d1b99b5fa0abed5dfb0b22eecb0dff6b1e0e34
홍서준 <auddml321@hanyang.ac.kr>
2026-02-03 13:08:37 +0900

    boj_1260_BFS와DFS

--- src/Baekjoon/boj_1260_DFS와BFS.java
+++ src/Baekjoon/boj_1260_DFS와BFS.java
@@ -7,7 +7,7 @@ public class boj_1260_DFS와BFS {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken()); 
 		int M = Integer.parseInt(st.nextToken());
 		int start = Integer.parseInt(st.nextToken());
 		Deque<Integer> Q = new ArrayDeque<>();
@@ -25,27 +25,30 @@ public class boj_1260_DFS와BFS {
 			gr[x].add(y);
 			gr[y].add(x);
 		}
-		
+		for(int i = 1; i<=N; i++) {
+			gr[i].sort(null);
+		}
 		
 		
 
 		int[]stkvis = new int[N+1];
 		stk.push(start);
-		stkvis[start] = 1;
-		stksb.append(start).append(" ");
-		while(!stk.isEmpty()) {
-			int cur = stk.pop();
-			for(int i = 0; i<gr[cur].size(); i++) {
-				if(stkvis[gr[cur].get(i)] != 0) continue;
-				stk.push(gr[cur].get(i));
-				stkvis[gr[cur].get(i)] = 1;
-				stksb.append(gr[cur].get(i)).append(" ");
-			}
+//		stkvis[start] = 1;
+		while (!stk.isEmpty()) {
+		    int cur = stk.pop();
+		    if (stkvis[cur] != 0) continue;
+		    stkvis[cur] = 1;
+		    stksb.append(cur).append(' ');
+
+		    for (int i = gr[cur].size() - 1; i >= 0; i--) {
+		        int nxt = gr[cur].get(i);
+		        if (stkvis[nxt] ==0) stk.push(nxt);
+		    }
 		}
 		System.out.println(stksb);
 		
 		int[]Qvis = new int[N+1];
-		Q.offer(start);
+		Q.offer(start); 
 		Qvis[start] = 1;
 		Qsb.append(start).append(" ");
 		while(!Q.isEmpty()) {
