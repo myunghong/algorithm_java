fd8c57a6f4d5052464060f057e55509c844bdfce
홍서준 <auddml321@hanyang.ac.kr>
2026-02-12 13:00:06 +0900

    boj_11286_절대값힙

--- src/Baekjoon/boj_11286_절대값힙.java
+++ src/Baekjoon/boj_11286_절대값힙.java
@@ -1,10 +1,39 @@
 package Baekjoon;
-
+import java.util.*;
+import java.io.*;
 public class boj_11286_절대값힙 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> minpq = new PriorityQueue<>(); // 양수
+		PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder()); //음수
+		for(int i = 0; i<N; i++) {
+			int temp = Integer.parseInt(br.readLine());
+			if(temp < 0) maxpq.offer(temp);
+			else if (temp > 0)minpq.offer(temp);
+			if(temp == 0) {
+				if(maxpq.isEmpty() && minpq.isEmpty()) {
+					System.out.println("0");
+				}
+				else if(maxpq.isEmpty() && !minpq.isEmpty()) {
+					System.out.println(minpq.poll());
+				}
+				else if(!maxpq.isEmpty() && minpq.isEmpty()) {
+					System.out.println(maxpq.poll());
+				}
+				else {
+					
+					int a = Math.abs(maxpq.peek());
+					int b = Math.abs(minpq.peek());
+					if(a == b) System.out.println(maxpq.poll());
+					else if(a > b) System.out.println(minpq.poll());
+					else if(a < b) System.out.println(maxpq.poll());
+				}
+				
+			}
+		}
 	}
 
 }
--- src/SWEA/swea_2382_미생물격리.java
+++ src/SWEA/swea_2382_미생물격리.java
@@ -1,10 +1,35 @@
 package SWEA;
+import java.util.*;
+import java.io.*;
 
 public class swea_2382_미생물격리 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int TC = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc<= TC; tc++) {
+			sb.append("#").append(tc).append(" ");
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken()); // 배열 크기
+			int M = Integer.parseInt(st.nextToken()); // M시간 후
+			int K = Integer.parseInt(st.nextToken()); // 군집 수
+			sangmul[][] board = new sangmul[N][N];
+			for(int i = 0; i<K; i++	) {
+				st = new StringTokenizer(br.readLine());
+				board[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = new sangmul(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+		}
 
 	}
-
+	static class sangmul{
+		int amount;
+		int dir;
+		sangmul(int amount, int dir){
+			this.amount = amount;
+			this.dir = dir;
+		}
+	}
 }
