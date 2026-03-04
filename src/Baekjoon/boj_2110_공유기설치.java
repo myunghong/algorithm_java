11c95c3a23fabd92e89109a7ac4160b09897768e
홍서준 <auddml321@hanyang.ac.kr>
2026-03-04 09:28:28 +0900

    boj_2110_공유기 설치 찾는 정답값에 이분탐색을 사용할 수도 있구만..

--- src/Baekjoon/boj_2110_공유기설치.java
+++ src/Baekjoon/boj_2110_공유기설치.java
@@ -18,25 +18,27 @@ public class boj_2110_공유기설치 {
 		}
 		int st = 0, ans = 0;
 		int en = board[N-1];
-		while(st > en) {
-			int mid = (st + en) / 2;
-			int cnt = 0;
+		while(st <= en) {
+			int mid = (st + en) / 2; //정답 후보
+			int cnt = 1;
 			int temp = 0;
 			for(int i = 0; i<N-1; i++) {
 				temp += board[i+1] - board[i];
-				if(temp > ans) {
-					ans = temp
+				if(temp >= mid) {
+					temp = 0;
+					cnt++;
 				}
-				cnt++;
-				if(cnt >= C) {
-					st = mid + 1;
-				}
-				else {
-					en = mid - 1;
-				}
+			}
+			if(cnt >= C) {
+				ans = Math.max(mid, ans);
+				st = mid + 1;
+			}
+			else {
+				en = mid - 1;
 			}
 		}
 		//board = 1, 2, 4, 8, 9
 		//distance = 1, 2, 4, 1
+		System.out.println(ans);
 	}
 }
