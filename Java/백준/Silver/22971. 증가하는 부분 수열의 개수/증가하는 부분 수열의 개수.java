269cade3226589c3c88b84522a873ccda06634cc
seohong <143954291+myunghong@users.noreply.github.com>
2026-03-31 17:26:58 +0900

    [Silver II] Title: 증가하는 부분 수열의 개수, Time: 256 ms, Memory: 14264 KB -BaekjoonHub

--- Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
+++ Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
@@ -6,16 +6,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] board= new int[5001];
-		Long[] ans = new Long[N];
-		Long[] ans1 = new Long[5001];
+		int[] board= new int[N];
+		int[] ans = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i = 0; i<N; i++) {
 			board[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.fill(ans, 1l);
-		Arrays.fill(ans1, 0l);
+		Arrays.fill(ans, 1);
 		for(int i = 1; i<N; i++) {
 			for(int j = 0; j<i; j++) {
 				if(board[i] > board[j]) ans[i] += ans[j];
